5f3d4914cb66942069d1af28f18c3ed8bbc8cf17
Teistt <80969848+Teistt@users.noreply.github.com>
2021-08-18 11:34:39 +0200

    Update CardsBP.cs

--- Assets/Script/CardsBP.cs
+++ Assets/Script/CardsBP.cs
@@ -3,11 +3,9 @@ using UnityEngine;
 [System.Serializable]
 public class CardsBP
 {
-    //Test commit git
-
     public int id;
     public string word;
-    //Enregistre l'ID de la team qui a gagné cette carte pour chacune des manches
+    //Enregistre l'ID de la team qui a gagnÃ© cette carte pour chacune des manches
     public int M1_win;
     public int M2_win;
     public int M3_win;
